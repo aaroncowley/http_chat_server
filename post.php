8b413e8b81dca37e479dcf9be35364fb7a8a8e1d
aaroncowley <acow777@gmail.com>
2017-12-05 11:52:18 -0700

    everything is now working

--- post.php
+++ post.php
@@ -2,9 +2,25 @@
 session_start();
 if(isset($_SESSION['name'])){
     $text = $_POST['text'];
+    $command = explode(" ",$_POST['text']);
+
+    $contents;
+    $dir = 'images/';
+    if ($dh = opendir($dir)){
+        while (($file = readdir($dh)) !== false){
+          $contents .=  $file.", ";
+      }
+    }
      
     $fp = fopen("log.html", 'a');
     fwrite($fp, "<div class='msgln'>(".date("g:i A").") <b>".$_SESSION['name']."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
+    if ($command[0] == "/get"){
+        if (sizeof($command) != 2){
+            fwrite($fp, "<div class='help'>your available files to /get are: ".$contents."</div>");
+        }else{
+            fwrite($fp, "<div class='image'> <img src='images/".$command[1]."' alt='maymay'></div>"); 
+        }
+    }    
     fclose($fp);
 }
 ?>
